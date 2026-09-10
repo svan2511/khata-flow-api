b6cb995fab0d7403dfbc5117f3b7f100c7199c4c
Anil Kumar <anilkumarsikwal25@gmail.com>
2026-09-10 18:39:44 +0530

    changes in entry file

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,8 +29,8 @@ echo "Running database migrations..."
 php artisan migrate:fresh --force
 
 # Seed Restaurant Menu
-echo "Seeding restaurant menu..."
-php artisan db:seed --class=RestaurantMenuSeeder --force
+# echo "Seeding restaurant menu..."
+# php artisan db:seed --class=RestaurantMenuSeeder --force
 
 
 # ================== PASSPORT KEYS SETUP ==================
